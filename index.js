9a6ee53d7fb0bb66b464fe10dec9e81cf3899984
Andrew <ondralukes06@seznam.cz>
2023-12-18 14:09:00 +0100

    fixing typos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import stylesheets
- 
+
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
@@ -13,6 +13,31 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+// Check if the device has a touch screen
+function isTouchDevice() {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+  }
+
+  // Add or remove the 'hovered' class based on touch screen detection
+  function handleTouch() {
+    const portfolioItems = document.querySelectorAll('.portfolio__item');
+
+    if (isTouchDevice()) {
+      portfolioItems.forEach(item => {
+        item.addEventListener('touchstart', () => {
+          item.classList.add('hovered');
+        });
+
+        item.addEventListener('touchend', () => {
+          item.classList.remove('hovered');
+        });
+      });
+    }
+  }
+
+  // Run the function when the page is loaded
+  document.addEventListener('DOMContentLoaded', handleTouch);
 
 // zobrazit fotky affiliate produktů
 // zmenšit text a upravit v affiliate sekci
